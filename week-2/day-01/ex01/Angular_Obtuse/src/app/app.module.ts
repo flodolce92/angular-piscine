d1098b21c7a9d6fb825e6dc126b47928bb41151e
Federico Lo Dolce <flodolce92@gmail.com>
2024-04-30 03:17:17 +0200

    day-01 ex01 boh

--- week-2/day-01/ex01/Angular_Obtuse/src/app/app.module.ts
+++ week-2/day-01/ex01/Angular_Obtuse/src/app/app.module.ts
@@ -4,7 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { TreeComponent } from './tree/tree.component';
 import {MatTreeModule} from '@angular/material/tree';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -13,7 +16,10 @@ import {MatTreeModule} from '@angular/material/tree';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatTreeModule
+    TreeComponent,
+    MatTreeModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [
     provideAnimationsAsync()
