e2e7155b83d92cab93111efa1d6a36bcbefbfe55
Federico Lo Dolce <flodolce92@gmail.com>
2024-05-12 18:39:35 +0200

    day-04 ex00 v1

--- week-2/day-04/ex00/ROUTE66/src/app/app-routing.module.ts
+++ week-2/day-04/ex00/ROUTE66/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { InsertComponent } from './insert/insert.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'home', component: HomeComponent },
+	{ path: 'product', component: ProductComponent },
+	{ path: 'insert', component: InsertComponent },
+	{ path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- week-2/day-04/ex00/ROUTE66/src/app/app.module.ts
+++ week-2/day-04/ex00/ROUTE66/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { InsertComponent } from './insert/insert.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    ProductComponent,
+    InsertComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
