8b405139009e1c4573529959978a6b501f70575d
Federico Lo Dolce <flodolce92@gmail.com>
2024-05-12 19:53:12 +0200

    day-04 ex00 end

--- week-2/day-04/ex00/ROUTE66/src/app/menu/menu.component.ts
+++ week-2/day-04/ex00/ROUTE66/src/app/menu/menu.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+	selector: 'app-menu',
+	templateUrl: './menu.component.html',
+	styleUrl: './menu.component.css',
 })
 export class MenuComponent {
+	constructor(private router: Router) {}
 
+	isActive(route: string): boolean {
+		return this.router.url === route;
+	}
 }
